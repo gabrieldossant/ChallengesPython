f5736c0b034b1e970f20371bbf7d60f2158c7270
Gabriel dos Santos Silva <80858391+gabrieldossant@users.noreply.github.com>
2023-03-23 14:14:33 -0300

    Delete Challenge6.py

--- Challenge6.py
+++
@@ -1,8 +0,0 @@
-import math
-
-value = float(input("Type a number: "))
-
-if value < 0:
-    print("The result is negative: ", math.sqrt(value)
-else:
-    print("The result is positive: ", math.sqrt(value)
