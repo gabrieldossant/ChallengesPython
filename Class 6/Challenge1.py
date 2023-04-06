d0ddee6e3e21744972a66009251637e1a8f556fd
Gabriel dos Santos Silva <gabrieldossantossilva69@gmail.com>
2023-04-05 21:31:42 -0300

    Add challenge6

--- Class 6/Challenge1.py
+++ Class 6/Challenge1.py
@@ -1,3 +1,4 @@
 num = int(input("Type a number: "))
+c = 1
 for i in range(11):
     print(f"{num} * {i} = {num*i}")
---
+++ Class 6/Challenge3.py
@@ -0,0 +1,7 @@
+result = 0
+
+for i in range(10):
+    number = float(input(f"Type {i+1} number: "))
+    result += number
+
+print(f"The result is {result}")
