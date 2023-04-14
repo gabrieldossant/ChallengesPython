0404e2915b44b1dcac7508758ad13c930a7dfe85
Gabriel dos Santos Silva <gabrieldossantossilva69@gmail.com>
2023-04-14 12:54:09 -0300

    add Challenge6

--- Class 7/Challenge5.py
+++ Class 7/Challenge5.py
@@ -1,3 +1,11 @@
+'''
+    Lucas is taking the subject of Programming Language and will make an 
+    algorithm (flowchart) that will calculate the final grade of the subject 
+    and verify the final condition, according to the table. Also, considering 
+    that there will be five activities that make up 50% of the grade and the 
+    other 50% will be a (single) Assessment. How will this algorithm work? 
+    Use your knowledge of abstraction, algorithms, and problem decomposition
+'''
 activityNote = float(input("Type the activity note: "))
 examGrade = float(input("Type the exam grade: "))
 sum = 0
---
+++ Class 7/Challenge6.py
@@ -0,0 +1,13 @@
+'''
+    Paulo wants to know how many minimum wages he receives, but he only knows 
+    the amount of his salary.Make an algorithm (flowchart) that helps him find 
+    out. Use your knowledge of abstraction, algorithms, and problem decomposition
+'''
+
+mySalary = float(input("Enter the value of your salary: "))
+minimumSalary = 1253
+result = 0
+
+result = mySalary/minimumSalary
+
+print("Total of salaries received %.2f " %result)
