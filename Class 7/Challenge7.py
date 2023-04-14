2f925f5d63b99d1b3d28bda154a09f3a474bc9d2
Gabriel dos Santos Silva <gabrieldossantossilva69@gmail.com>
2023-04-14 16:41:10 -0300

    hipotenusa comment

--- Class 7/Challenge7.py
+++ Class 7/Challenge7.py
@@ -1,5 +1,13 @@
 import math
 
+'''
+    Maria estava fazendo a sua lição de casa queprecisava calcular o valor 
+    da hipotenusa de umtriângulo e teve a ideia de montar um algoritmo
+    (fluxograma) que faça isso mediante o valor doscatetos do triângulo. 
+    Como será esse algoritmo?Utilize o seu conhecimento em abstração, 
+    algoritmo edecomposição de problemas
+'''
+
 catet1 = float(input("Enter the value of the first catet: "))
 catet2 = float(input("Enter the value of the second catet: "))
 
