34e36d42786f36f4d624741a6e1c79402979cfbb
Gabriel dos Santos Silva <gabrieldossantossilva69@gmail.com>
2023-04-14 16:39:03 -0300

    Hipotenusa

---
+++ Class 7/Challenge7.py
@@ -0,0 +1,8 @@
+import math
+
+catet1 = float(input("Enter the value of the first catet: "))
+catet2 = float(input("Enter the value of the second catet: "))
+
+hipotenusa = math.sqrt(catet1**2 + catet2**2)
+
+print(f"The result of the hipotenusa is {hipotenusa}")
