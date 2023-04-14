e17fc9d3871a127eead7736efba41d6c761337fa
Gabriel dos Santos Silva <gabrieldossantossilva69@gmail.com>
2023-04-14 11:31:36 -0300

    Challenge5

---
+++ Class 7/Challenge5.py
@@ -0,0 +1,18 @@
+activityNote = float(input("Type the acitivity note: "))
+examGrade = float(input("Type the exam grade: "))
+sum = 0
+average = 0
+
+if activityNote >= 0 and activityNote <= 10:
+    sum += activityNote
+elif examGrade >= 0 and examGrade <= 10:
+    sum += examGrade
+
+average = sum/2
+
+if average >= 0 and average < 4:
+    print(f"The average students is {average} \n REPROVED")
+elif average >= 4 and average < 7:
+    print(f"The average students is {average} \n EXAM")
+elif average >= 7 and average <= 10:
+    print(f"The average students is {average} \n APROVED")
