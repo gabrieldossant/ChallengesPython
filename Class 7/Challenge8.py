818d915c93b6b8e8b87894bc77b9859111182dd2
Gabriel dos Santos Silva <gabrieldossantossilva69@gmail.com>
2023-04-22 18:21:07 -0300

    desenvolvendo

---
+++ Class 7/Challenge8.py
@@ -0,0 +1,19 @@
+n1 = float(input("Type the first number: "))
+n2 = float(input("Type the second number: "))
+choose = int(input("Choose the operator: [1/4]"))
+
+def verifyChoose(number, x, y):
+    if number == 1:
+        total = (x + y)/2
+        return total
+    elif number == 2:
+        if x > y:
+            print(f"The first number typed {x} is biggest than second number typed {y}.")
+        elif x < y:
+            print(f"The second number typed {y} is biggest than first number typed {x}.")
+        else:
+            print(f"The two numbers typed is are equal.")
+    elif number == 3:
+        
+
+result = verifyChoose(choose, n1, n2)
