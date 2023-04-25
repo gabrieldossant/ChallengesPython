68270a8e0d302d3469990389ab32dd3dcf2b869b
Gabriel dos Santos Silva <gabrieldossantossilva69@gmail.com>
2023-04-25 13:20:13 -0300

    development

--- Class 7/Challenge8.py
+++ Class 7/Challenge8.py
@@ -8,12 +8,13 @@ def verifyChoose(number, x, y):
         return total
     elif number == 2:
         if x > y:
-            print(f"The first number typed {x} is biggest than second number typed {y}.")
+            return print(f"The first number typed {x} is biggest than second number typed {y}.")
         elif x < y:
-            print(f"The second number typed {y} is biggest than first number typed {x}.")
+            return print(f"The second number typed {y} is biggest than first number typed {x}.")
         else:
-            print(f"The two numbers typed is are equal.")
+            return print(f"The two numbers typed is are equal.")
     elif number == 3:
+        total = x * y
         
 
 result = verifyChoose(choose, n1, n2)
