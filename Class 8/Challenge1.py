f7b726430b8979e8716d3df3b49875cc7e92932c
Gabriel dos Santos Silva <gabrieldossantossilva69@gmail.com>
2023-05-03 20:12:05 -0300

    2 desafio

---
+++ Class 8/Challenge1.py
@@ -0,0 +1,26 @@
+def exibirMensagem():
+    print("Olá, Mundo!")
+    print("Aprendendo funções.")
+
+exibirMensagem()
+
+print("-------------------------------------------")
+
+def spam(x):
+    return x
+
+print(spam("Passando parametro!"))
+
+print("-------------------------------------------")
+
+def dobro(x):
+    return x*2
+
+print(dobro(2))
+
+print("------------------------------------------")
+
+def tabuada(num):
+    return num, num*2, num*3, num*4, num*5, num*6
+
+print(tabuada(2))
---
+++ Class 8/Challenge2.py
@@ -0,0 +1,7 @@
+num1 = float(input("Digite um número: "))
+num2 = float(input("Digite outro número: "))
+
+def summ(x, y):
+    return x+y
+
+print(f"A soma dos valores é {summ(num1, num2)}")
