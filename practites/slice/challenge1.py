7484b98f889328984b7f438420404b479e055df9
Gabriel dos Santos Silva <gabrieldossantossilva69@gmail.com>
2023-05-11 16:19:18 -0300

    praticas

---
+++ practites/slice/challenge1.py
@@ -0,0 +1,8 @@
+# Crie uma função que receba uma string como parâmetro e retorne apenas os caracteres nas posições ímpares.
+
+def show(a, b, c, d, e):
+    for i in show:
+        if i%2 == 1:
+            print(i)
+            
+show(1, 2, 3, 4, 5)
