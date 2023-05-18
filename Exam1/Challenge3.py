ed76cd156fbd26d73fbf7050df46b56221c4be0b
Gabriel dos Santos Silva <gabrieldossantossilva69@gmail.com>
2023-05-17 23:58:38 -0300

    bug resolvido

--- Exam1/Challenge3.py
+++ Exam1/Challenge3.py
@@ -8,20 +8,18 @@ somaTotal = 0
 for i in range(200):
     numero = float(input(f"Digite {i+1} número: "))
     num = numero % 2
-    c = +i
-    qtd = +i
     if num == 0:
         somaPar += numero
         qtdPar += 1
         somaTotal = somaPar + somaImpar
+        qtd = qtd + 1
     else:
         somaImpar += numero
         qtdImpar += 1
         somaTotal = somaPar + somaImpar
+        qtd = qtd + 1
 
     if qtdImpar == 3:
-        if qtd < i:
-            qtd = +i
         print(f"A soma dos números pares: {somaPar}")
         print(f"Quantidade de números pares {qtdPar}")
         print(f"A soma dos números impares: {somaImpar}")
